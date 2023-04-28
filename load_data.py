151cc79d44d6b39bcb6ead47f6df71503de3b83a
Vansh Mahendra <vanshmahendra125@gmail.com>
2023-04-29 02:28:54 +0530

    updated

--- load_data.py
+++ load_data.py
@@ -1,48 +1,61 @@
+import tensorflow as tf
 import numpy as np
-from tqdm import tqdm
-from sklearn.utils import shuffle
-from skimage.io import imread, imshow
-from skimage.transform import resize
-from skimage.transform import resize
-import matplotlib.pyplot as plt
 import cv2
 import os
+from glob import glob
 
-def load_path(train_path, train_mask_path, test_path, test_mask_path):
-    train_x = [f for f in os.listdir(train_path)]
-    train_y = [f for f in os.listdir(train_mask_path)]
-    test_x = [f for f in os.listdir(test_path)]
-    test_y = [f for f in os.listdir(test_mask_path)]
-    return train_x, train_y, test_x, test_y
+h = 256
+w = 256
 
-def read_image(path, h, w):
+def load_path(x, y):
+    x = sorted(glob(os.path.join(x, "*")))
+    y = sorted(glob(os.path.join(y, "*")))
+    return x, y
+
+def read_image(path):
     img = cv2.imread(path, cv2.IMREAD_COLOR)
-    img = img/255.0  
     img = cv2.resize(img, (h, w))
+    img = img/255.0
+    img = img.astype(np.float32)
     return img
 
-def read_mask(path, h, w):
+def read_mask(path):
     mask = cv2.imread(path)
-    mask = cv2.resize(mask, (w, h))
+    mask = cv2.resize(mask, (h, w))
     mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     ret, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    mask = mask / 255.0 
     mask = mask[:, :, None]
+    mask = mask.astype(np.bool)
     return mask
 
-def get_data(x_path, y_path, x_ids, y_ids, h, w):
-    #getting Train images
-    X = np.zeros((len(x_ids), h, w, 3), dtype=np.float32)
-    Y = np.zeros((len(y_ids), h, w, 1), dtype=bool)
-    #Image
-    for n, id_ in tqdm(enumerate(x_ids), total=len(x_ids)):
-        path = x_path+'/'+id_
-        X[n] = read_image(path, h, w)
-    #Mask
-    for n, id_ in tqdm(enumerate(y_ids), total=len(y_ids)):
-        path = y_path+'/'+id_
-        Y[n] = read_mask(path, h, w)
-    return X, Y
+def get_data(x_path, y_path):
+    def get_each(x, y):
+        x = read_image(x.decode())
+        y = read_mask(y.decode())
+        return x, y
 
-def shuffling(x, y):
-    x, y = shuffle(x, y, random_state=42)
+    x, y = tf.numpy_function(get_each, [x_path, y_path], [tf.float32, tf.bool])
+    x.set_shape([h, w, 3])
+    y.set_shape([h, w, 1])
     return x, y
+
+def tf_data(x, y, buffer_size = 100, batch_size = 4, num_epochs = 1):
+    dataset = tf.data.Dataset.from_tensor_slices((x, y))
+    dataset = dataset.map(get_data)
+    dataset = dataset.shuffle(buffer_size = buffer_size)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.repeat(num_epochs)
+    dataset = dataset.prefetch(4)
+    return dataset
+
+def get_np_images(X_path, Y_path):
+    X = np.zeros((len(X_path), h, w, 3), dtype=np.float32)
+    Y = np.zeros((len(Y_path), h, w, 1), dtype=bool)
+    for idx, x in enumerate(X_path):
+        img = read_image(x)
+        X[idx] = img
+    for idx, y in enumerate(Y_path):
+        mask = read_mask(y)
+        Y[idx] = mask
+    return X, Y
